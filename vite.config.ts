50d9343f778ec4de2446fcb7a8cfe26c5903747d
Nyasper <herreragonzalo39@gmail.com>
2024-11-16 01:14:45 -0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { fileURLToPath, URL } from 'node:url';
+// import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'node:path';
 
 export default defineConfig({
 	plugins: [vue()],
 	resolve: {
 		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			'@': resolve(__dirname, 'src/'),
 		},
 	},
 });
