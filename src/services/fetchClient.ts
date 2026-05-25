139cc01c36ecbfc24a92e5221c8a5753b4f0f74b
Nyasper <herreragonzalo39@gmail.com>
2026-05-25 10:12:06 -0300

    env_updated

--- src/services/fetchClient.ts
+++ src/services/fetchClient.ts
@@ -1,50 +1,50 @@
-import { FetchError } from './fetchError';
+import { FetchError } from "./fetchError";
 
 const mode = import.meta.env.MODE;
 
-const devHost = 'http://localhost:3000';
+const serverHost =
+  mode === "development"
+    ? "http://localhost:3000"
+    : import.meta.env.VITE_PROD_HOST;
 
-const prodHost: string | undefined = import.meta.env.VITE_PROD_HOST;
-
-if (mode === 'production' && !prodHost) {
-	throw new Error("'VITE_PROD_HOST' env variable not provided");
+if (mode === "production" && serverHost === "http://localhost:3000") {
+  throw new Error("'VITE_PROD_HOST' env variable not provided");
 }
 
-const host: string = mode === 'production' ? prodHost! : devHost;
-const apiUrl = '/api/v1';
+const apiBaseUrl = "/api/v1";
 
 export async function fetchClient<T, B = undefined>(
-	endpoint: string,
-	method: HttpMetod = 'GET',
-	body?: B
+  endpoint: string,
+  method: HttpMetod = "GET",
+  body?: B,
 ): Promise<{ response: T; statusCode: number }> {
-	try {
-		const res = await fetch(`${host}${apiUrl}${endpoint}`, {
-			credentials: 'include',
-			mode: 'cors',
-			method,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: body ? JSON.stringify(body) : undefined,
-		});
-		const statusCode = res.status;
+  try {
+    const res = await fetch(`${serverHost}${apiBaseUrl}${endpoint}`, {
+      credentials: "include",
+      mode: "cors",
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body ? JSON.stringify(body) : undefined,
+    });
+    const statusCode = res.status;
 
-		if (!res.ok) {
-			const errorResponse = await res.json().catch(() => null);
-			throw new FetchError(
-				errorResponse?.message || 'Unexpected error',
-				statusCode
-			);
-		}
-		const response = (await res.json()) as T;
-		return { response, statusCode: res.status };
-	} catch (error) {
-		if (error instanceof FetchError) {
-			throw error; // Propagar errores FetchError directamente
-		}
-		throw new FetchError("Server doesn't respond", 500);
-	}
+    if (!res.ok) {
+      const errorResponse = await res.json().catch(() => null);
+      throw new FetchError(
+        errorResponse?.message || "Unexpected error",
+        statusCode,
+      );
+    }
+    const response = (await res.json()) as T;
+    return { response, statusCode: res.status };
+  } catch (error) {
+    if (error instanceof FetchError) {
+      throw error; // Propagar errores FetchError directamente
+    }
+    throw new FetchError("Server doesn't respond", 500);
+  }
 }
 
 // getter for endpoints
@@ -52,4 +52,4 @@ export const authBase = (endpoint: string) => `/auth${endpoint}`;
 export const notesBase = (endpoint: string) => `/notes${endpoint}`;
 export const adminUsersBase = (endpoint: string) => `/admin/users${endpoint}`;
 
-type HttpMetod = 'GET' | 'POST' | 'PUT' | 'DELETE';
+type HttpMetod = "GET" | "POST" | "PUT" | "DELETE";
